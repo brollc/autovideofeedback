19883d8d4971946dade69fef34893cc46de9a306
Brian Broll <brian.broll@gmail.com>
2018-07-07 18:46:50 -0500

    initial pass at integrating shepherdjs

--- js/example.js
+++ js/example.js
@@ -3,4 +3,62 @@ SETTINGS.VIDEO_URL = 'content/example.mp4'
 SETTINGS.END_TIME = 20;
 SETTINGS.PROMPT_INTERVAL = 11;
 SETTINGS.INITIAL_DELAY = 7.5;
-var player = new VideoPrompter(document.getElementById('replayer'), SETTINGS);
+const player = new VideoPrompter(document.getElementById('replayer'), SETTINGS);
+const initTour = new Shepherd.Tour({
+  defaults: { 
+    classes: 'shepherd-theme-arrows',
+    scrollTo: true
+  }
+});
+
+initTour.addStep('intro', {
+    text: 'Hello and welcome to the video replayer thing!',
+        //'Before we start with your video, we will introduce the ',
+    buttons: [
+        {
+            text: 'Next',
+            action: initTour.next
+        }
+    ]
+});
+
+initTour.addStep('background-click', {
+    text: 'Click anywhere on the video to start',
+        //'Before we start with your video, we will introduce the ',
+    buttons: [
+        {
+            text: 'Next',
+            action: initTour.next
+        }
+    ]
+});
+
+//initTour.addStep('background-click', {
+    //text: 'After the video starts, you will view',
+        ////'Before we start with your video, we will introduce the ',
+    //buttons: [
+        //{
+            //text: 'Next',
+            //action: initTour.next
+        //}
+    //]
+//});
+
+
+initTour.start();
+// Show an initial explanation of clicking on the video
+// TODO
+
+player.on('end', function() {
+    console.log('DONE!');
+    // Show the final message explaining that the video is now complete.
+    // Link to the actual video.
+    // TODO
+});
+
+player.on('prompt', function(count) {
+    //tour.start();
+    console.log('prompt #', count);
+    // Explain the prompts
+    // TODO
+});
